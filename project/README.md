429e698899a1aff0a34e70207fc5746a50abd947
weichee98 <56989558+weichee98@users.noreply.github.com>
2021-11-20 08:57:35 +0800

    Added Project (#2)
    * update gitignore, added readme and requirements.txt
    
    * added project images
    
    * Create otsu.py
    
    * added image text reference
    
    * update readme and requirements.txt
    
    * update scripts
    
    * remove grabcut
    
    * output threshold map
    
    * Added lab report
    
    * added regression thresholding
    
    * rename otsu.py to thresholding.py
    
    * Added sample 03
    
    * convert sample 03 to png
    
    * added timeit function and polynomial regression
    
    * refactor project structure
    
    * added logging for OCR
    
    * Added function to plot OTSU histogram
    
    * Added report

---
+++ project/README.md
@@ -0,0 +1,15 @@
+# Text Image Segmentation for Optimal Optical Character Recognition
+
+## Setup
+
+```shell
+conda install --file requirements.txt -c conda-forge
+```
+
+## Reference
+
+1. [Enhance a Document Scan using Python and OpenCV](https://medium.com/analytics-vidhya/enhance-a-document-scan-using-python-and-opencv-9934a0c2da3d)
+
+
+
+
---
+++ project/thresholding.py
@@ -0,0 +1,247 @@
+
+import cv2
+import time
+import logging
+import numpy as np
+from abc import abstractmethod
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.pipeline import Pipeline
+
+
+def _timeit(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        res = func(*args, **kwargs)
+        end = time.time()
+
+        func_name = "{}.{}".format(
+            args[0].__name__,
+            func.__name__
+        )
+        logging.info("{} time: {:.5f} s".format(func_name, end - start))
+        for k, v in kwargs.items():
+            logging.info("{} kwargs: {}: {}".format(func_name, k, v))
+        logging.info("")
+        return res
+    return wrapper
+
+
+class _BaseThresholding:
+
+    @classmethod
+    @abstractmethod
+    def threshold(cls, img, **kwargs):
+        """
+        To find the threshold used for binarization of a grayscale image
+
+        Parameters
+        ----------
+        img: np.array (height, width)
+            The grayscale image
+
+        Returns
+        -------
+        threshold: np.array (height, width)
+            The threshold at each pixel position
+
+        """
+        raise NotImplementedError
+
+    @classmethod
+    @_timeit
+    def binarize(cls, img, return_threshold=True, **kwargs):
+        """
+        To binarize a grayscale image
+
+        Parameters
+        ----------
+        img: np.array (height, width)
+            The grayscale image
+        return_threshold: bool
+            Whether or not to return threshold map
+
+        Returns
+        -------
+        new_img: np.array (height, width)
+            The binarized image
+
+        """
+        t = cls.threshold(img, **kwargs)
+        b = np.where(img >= t, 255, 0).astype(np.uint8)
+        if return_threshold:
+            return b, t
+        return b
+
+
+class OTSU(_BaseThresholding):
+    """
+    Perform global OTSU thresholding
+    """
+
+    @classmethod
+    def plot_histogram(cls, img, output_path=None, title=None):
+        histogram = cv2.calcHist(
+            images=[img],
+            channels=[0],
+            mask=None,
+            histSize=[256],
+            ranges=[0, 256],
+        )
+
+        num_pixels = img.size
+        probabilities = histogram.flatten() / num_pixels
+        gray_levels = np.arange(len(probabilities))
+        mean = gray_levels * probabilities
+
+        intra_class_variance = []
+        variance_thresholds = []
+        best_threshold = -1
+        min_variance = float('inf')
+
+        for t in range(1, len(histogram)):
+            lower_probability = np.sum(probabilities[:t])
+            upper_probability = np.sum(probabilities[t:])
+            if lower_probability == 0 or upper_probability == 0:
+                continue
+
+            lower_mean = np.sum(mean[:t]) / lower_probability
+            upper_mean = np.sum(mean[t:]) / upper_probability
+
+            lower_variance = np.sum(
+                ((gray_levels[:t] - lower_mean) ** 2) * probabilities[:t]) / lower_probability
+            upper_variance = np.sum(
+                ((gray_levels[t:] - upper_mean) ** 2) * probabilities[t:]) / upper_probability
+
+            variance = lower_probability * lower_variance + upper_probability * upper_variance
+            intra_class_variance.append(variance)
+            variance_thresholds.append(t)
+            if variance < min_variance:
+                min_variance = variance
+                best_threshold = t
+
+        f, ax = plt.subplots(1, 1, figsize=(10, 5))
+        ax.hist(img.ravel(), bins=256, label="histogram", color="tab:blue", alpha=0.7)
+        ax.set_ylabel("histogram count")
+        ax.set_xlabel("pixel intensity / threshold (t)")
+
+        ax2 = ax.twinx()
+        ax2.plot(variance_thresholds, intra_class_variance, color="tab:orange", label="interclass variance")
+        ax2.axvline(best_threshold, color="red", linestyle="--")
+        plt.text(best_threshold - 5, np.mean(intra_class_variance), "best threshold", rotation=90)
+        ax2.set_ylabel("interclass variance at threshold (t)")
+
+        handles1, _ = ax.get_legend_handles_labels()
+        handles2, _ = ax2.get_legend_handles_labels()
+        ax.legend(handles=handles1 + handles2, loc="upper right", bbox_to_anchor=(1., -0.05))
+
+        plt.title(title)
+        plt.tight_layout()
+        if output_path is not None:
+            f.savefig(output_path)
+        return f
+
+    @classmethod
+    def threshold(cls, img, **kwargs):
+        histogram = cv2.calcHist(
+            images=[img],
+            channels=[0],
+            mask=None,
+            histSize=[256],
+            ranges=[0, 256],
+        )
+        num_pixels = img.size
+        probabilities = histogram.flatten() / num_pixels
+        gray_levels = np.arange(len(probabilities))
+        mean = gray_levels * probabilities
+
+        best_threshold = -1
+        min_variance = float('inf')
+
+        for t in range(1, len(histogram)):
+            lower_probability = np.sum(probabilities[:t])
+            upper_probability = np.sum(probabilities[t:])
+            if lower_probability == 0 or upper_probability == 0:
+                continue
+
+            lower_mean = np.sum(mean[:t]) / lower_probability
+            upper_mean = np.sum(mean[t:]) / upper_probability
+
+            lower_variance = np.sum(
+                ((gray_levels[:t] - lower_mean) ** 2) * probabilities[:t]) / lower_probability
+            upper_variance = np.sum(
+                ((gray_levels[t:] - upper_mean) ** 2) * probabilities[t:]) / upper_probability
+
+            variance = lower_probability * lower_variance + upper_probability * upper_variance
+            if variance < min_variance:
+                min_variance = variance
+                best_threshold = t
+
+        return np.full_like(img, fill_value=best_threshold)
+
+
+class AdaptiveMeanThresholding(_BaseThresholding):
+    """
+    Threshold for each pixel is the mean of its (K x K) neighbourhood
+    """
+
+    @classmethod
+    def threshold(cls, img, C=0, ksize=5, **kwargs):
+        mean = cv2.blur(img, ksize=(ksize, ksize))
+        t = np.where(mean > C, mean - C, 0).astype(np.uint8)
+        return t
+
+
+class RegressionThresholding(_BaseThresholding):
+    """
+    Thresholding using regression models
+    """
+    @classmethod
+    def _fit(cls, data, rgr_model, P, C):
+        P = int(P)
+
+        y = data[:, 2]
+        y_mean, y_std = np.mean(y), np.std(y)
+        y = (y - y_mean) / y_std
+
+        X_preprocess = [("scale", StandardScaler())]
+        if P > 1:
+            X_preprocess.append(("poly", PolynomialFeatures(P)))
+        X_preprocess = Pipeline(X_preprocess)
+        X = X_preprocess.fit_transform(data[:, :-1])
+
+        rgr_model.fit(X, y)
+        pred = rgr_model.predict(X) - C
+        inv_pred = (pred * y_std) + y_mean
+        threshold = np.clip(inv_pred, 0, 255)
+        return threshold
+
+    @classmethod
+    def threshold(cls, img, rgr_model=LinearRegression(), P=1, C=0, downsample=None, **kwargs):
+        if downsample is None or downsample <= 1:
+            _height, _width = img.shape[0], img.shape[1]
+            _img = img
+        else:
+            downsample = float(downsample)
+            _width, _height = int(img.shape[1] / downsample), int(img.shape[0] / downsample)
+            _img = cv2.resize(img, (_width, _height))
+
+        x = np.repeat(np.expand_dims(np.arange(_width), axis=0), _height, axis=0)
+        y = np.repeat(np.expand_dims(np.arange(_height), axis=1), _width, axis=1)
+        data = np.stack([x, y, _img], axis=2).reshape((-1, 3))
+
+        threshold = cls._fit(data, rgr_model, P, C)
+        threshold = threshold.reshape(_img.shape).astype(np.uint8)
+
+        if downsample is None or downsample <= 1:
+            return threshold
+        threshold = cv2.resize(threshold, (img.shape[1], img.shape[0]))
+        return threshold
+
+
+
+
